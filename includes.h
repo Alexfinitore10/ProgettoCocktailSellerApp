0099cc4c640cf3e62f304e8762aab5800143eb11
Elvino <el.buonanno@studenti.unina.it>
2023-02-21 12:38:41 +0100

    aggiunto header file con librerie di base

---
+++ includes.h
@@ -0,0 +1,16 @@
+#ifndef __INCLUDES_H_
+#define __INCLUDES_H_
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <math.h> 
+
+ 
+#endif
