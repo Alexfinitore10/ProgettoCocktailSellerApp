2541c0409d0e0cc1f58ac1b166183be1ad21ec36
Elvino00 <el.buonanno@studenti.unina.it>
2023-08-10 11:58:41 +0200

    Commit codice di test

--- includes.h
+++ includes.h
@@ -11,6 +11,6 @@
 #include <string.h>
 #include <pthread.h>
 #include <math.h> 
-
+#include <libpq-fe.h>
  
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,18 @@ void insert_cocktail(char[],double,int);
 void reduce_amount(char[],int);
 
 int main(){
-    printf("Hello World\n");
+    PGconn *conn;
+    conn = PQconnectdb("dbname = elvino user = elvino password = elvino host = 127.0.0.1 port = 5432");
+
+    if (PQstatus(conn) == CONNECTION_BAD){
+        PQfinish(conn);
+    }
+
+    int ver = PQserverVersion(conn);
+
+    printf("Server version %d\n",ver);
+
+    PQfinish(conn);
 }
 
 void reduce_amount(char nome[], int quantita)
