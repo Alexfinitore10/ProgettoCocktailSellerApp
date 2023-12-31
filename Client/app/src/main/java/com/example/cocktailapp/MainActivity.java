8c7dcaf24874589be237ed3e491eab7539a082c3
Elvino00 <el.buonanno@studenti.unina.it>
2023-12-31 12:15:13 +0100

    Create schermate di Login e Registrazione.

--- Client/app/src/main/java/com/example/cocktailapp/MainActivity.java
+++ Client/app/src/main/java/com/example/cocktailapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.cocktailapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupButton = findViewById(R.id.RegisterButton);
+
+        loginButton = findViewById(R.id.LoginButton);
+
+        signupButton.setOnClickListener(v -> {
+            startActivity(new Intent(this,SignUpActivity.class));
+        });
+
+        loginButton.setOnClickListener(v -> {
+               startActivity(new Intent(this,LoginActivity.class));
+        });
     }
+
+
+
 }
