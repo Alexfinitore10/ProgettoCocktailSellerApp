0ff27fcefba119ae7c255f2ad8cd1756536837c1
Alex <alexciacciarella@gmail.com>
2024-05-14 16:35:06 +0200

    Aggiustamenti piccolini

--- carrello.java
+++ carrello.java
@@ -32,24 +32,29 @@ public class carrello {
 
     // vedi tutti gli elementi del carrello
     public void viewItems() {
-        System.out.println("Cocktails nel carrello:");
+        System.out.println("+---------------+---------------------------------------------+-------------------+---------------+");
+        System.out.println("|   Cocktails   |                    Ingredienti              | Gradazione Alcolica|    Prezzo     |");
+        System.out.println("+---------------+---------------------------------------------+-------------------+---------------+");
         if (cocktails.isEmpty()) {
-            System.out.println("Non ci sono cocktail nel carrello.");
+            System.out.println("| Nessun cocktail nel carrello.                                              |");
         } else {
             for (Cocktail cocktail : cocktails) {
-                System.out.println(cocktail.getNome());
+                System.out.printf("| %-13s | %-21s | %-17.1f | %-13.2f |%n", cocktail.getNome(), cocktail.getIngredienti(), cocktail.getGradazioneAlcolica(), cocktail.getPrezzo());
             }
         }
-
-        System.out.println("Shake nel carrello:");
+        System.out.println("+---------------+---------------------------------------------+---------------+");
+        System.out.println("|     Shakes    |                    Ingredienti              |       Prezzo       |");
+        System.out.println("+---------------+---------------------------------------------+---------------+");
         if (shakes.isEmpty()) {
-            System.out.println("Non ci sono shake nel carrello.");
+            System.out.println("| Nessuno shake nel carrello.                                                 |");
         } else {
             for (Shake shake : shakes) {
-                System.out.println(shake.getNome());
+                System.out.printf("| %-13s | %-21s | %-17.2f |%n", shake.getNome(), shake.getIngredienti(), shake.getPrezzo());
             }
         }
+        System.out.println("+---------------+-----------------------+-------------------+---------------+");
     }
+    
 
     // Calcola il totale del carrello
     public float calculateTotal() {
