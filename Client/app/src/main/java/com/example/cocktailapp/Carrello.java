78b580586424330e3255c0e68a9c0300e28873de
Elvino00 <el.buonanno@studenti.unina.it>
2024-05-16 16:52:54 +0200

    metodo visualizzazzione carrello aggiornato

--- Client/app/src/main/java/com/example/cocktailapp/Carrello.java
+++ Client/app/src/main/java/com/example/cocktailapp/Carrello.java
@@ -39,26 +39,31 @@ public class Carrello {
         return shakes;
     }
 
-    // vedi tutti gli elementi del carrello
+
     public void viewItems() {
-        System.out.println("+---------------+---------------------------------------------+-------------------+---------------+");
-        System.out.println("|   Cocktails   |                    Ingredienti              | Gradazione Alcolica|    Prezzo     |");
-        System.out.println("+---------------+---------------------------------------------+-------------------+---------------+");
+        System.out.println(
+                "+---------------+---------------------------------------------+-------------------+---------------+");
+        System.out.println(
+                "|   Cocktails   |                    Ingredienti              | Gradazione Alcolica|    Prezzo     |");
+        System.out.println(
+                "+---------------+---------------------------------------------+-------------------+---------------+");
         if (cocktails.isEmpty()) {
             System.out.println("| Nessun cocktail nel carrello.                                              |");
         } else {
             for (Cocktail cocktail : cocktails) {
-                System.out.printf("| %-13s | %-21s | %-17.1f | %-13.2f |%n", cocktail.getNome(), cocktail.getIngredienti(), cocktail.getGradazione_alcolica(), cocktail.getPrezzo());
+                System.out.printf("| %-13s | %-21s | %-17.1f | %-13.2f |%n", cocktail.getNome(),
+                        cocktail.getIngredienti(), cocktail.getGradazione_alcolica(), cocktail.getPrezzo());
             }
         }
         System.out.println("+---------------+---------------------------------------------+---------------+");
-        System.out.println("|     Shakes    |                    Ingredienti              |       Prezzo       |");
+        System.out.println("|     Shakes    |                    Ingredienti              |       Prezzo  |");
         System.out.println("+---------------+---------------------------------------------+---------------+");
         if (shakes.isEmpty()) {
-            System.out.println("| Nessuno shake nel carrello.                                                 |");
+            System.out.println("| Nessuno shake nel carrello.                                             |");
         } else {
             for (Shake shake : shakes) {
-                System.out.printf("| %-13s | %-21s | %-17.2f |%n", shake.getNome(), shake.getIngredienti(), shake.getPrezzo());
+                System.out.printf("| %-13s | %-21s | %-17.2f |%n", shake.getNome(), shake.getIngredienti(),
+                        shake.getPrezzo());
             }
         }
         System.out.println("+---------------+-----------------------+-------------------+---------------+");
